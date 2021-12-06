16bbde2c8ccd9e78417fc2d37375bcbf81288195
Theo-Blaise <87185589+Theo-Blaise@users.noreply.github.com>
2021-12-06 17:41:15 +0100

    techifyFiles

---
+++ authHeader.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarIT" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarIT">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active " href="#">Dashboard</a>
+                </li>
+
+            </ul>
+
+            <ul class="nav justify-content-lg-end">
+               
+                <li class="nav-item">
+                    <a class="nav-link " href="student">Students</a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link " href="logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
